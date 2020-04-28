728d8eb7ce9575a9891d5e39816f89e4b9b119da
ttafsir <ttafsir@gmail.com>
2020-04-28 11:49:56 -0400

    Add requires

--- setup.py
+++ setup.py
@@ -3,6 +3,15 @@ from setuptools import find_packages, setup
 with open('README.md', 'r') as f:
     long_description = f.read()
 
+REQUIRES = [
+    "click",
+    "requests>=2.20.0",
+    "tabulate",
+    "python-dotenv"
+    "pyyaml",
+    "jinja2",
+]
+
 setup(
     name='evengsdk',
     version='1.0',
@@ -14,7 +23,7 @@ setup(
     url='',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    install_requires=['evengsdk', 'Click'],
+    install_requires=REQUIRES,
     entry_points={
         'console_scripts': [
             'evengcli=evengsdk.cli.cli:main'
