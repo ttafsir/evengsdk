359235f5ea6f58329b98b7c00ee8de341485aa1e
Tafsir Thiam <ttafsir@gmail.com>
2021-02-07 11:50:52 -0500

    Adds display help for status responses

--- src/evengsdk/cli/helpers.py
+++ src/evengsdk/cli/helpers.py
@@ -2,6 +2,9 @@
 from concurrent.futures import ThreadPoolExecutor
 import html
 from typing import Dict, List
+import sys
+
+import click
 
 
 def to_human_readable(
@@ -35,3 +38,13 @@ def thread_executor(func, items):
         for future in executor.map(func, items):
             futures.append(future)
     return futures
+
+
+def display_status(resp: Dict):
+    if resp:
+        msg = resp.get('message')
+        fg_color = 'green' if resp['status'] == 'success' else 'red'
+        click.secho(msg, fg=fg_color)
+        sys.exit(0)
+    err = click.style('Unknown Error: no status received', fg='red')
+    sys.exit(err)
