f57e66c7b0c3436bddd1862349c03cbb48b6a918
Tafsir Thiam <ttafsir@gmail.com>
2021-04-24 15:40:01 -0400

    Add tests/refactor failing

--- src/tests/test_api_lab.py
+++ src/tests/test_api_lab.py
@@ -1,18 +1,13 @@
 import pytest
 
-from evengsdk.client import EvengClient
-from requests.exceptions import HTTPError
+from evengsdk.exceptions import EvengHTTPError
 
 
 LAB_PATH = '/datacenter/'
 LAB_NAME = 'leaf_spine_lab'
 EXT = '.unl'
 
-DEVICE_UNDER_TEST = {
-    'host': '10.246.32.35',
-    'username': 'admin',
-    'password': 'eve'
-}
+
 TESTLAB = {
   'name': 'TestLab',
   'path': '/',
@@ -21,52 +16,74 @@ TESTLAB = {
   'body': 'longer description'
 }
 
-@pytest.fixture()
-def client():
-    client = EvengClient(
-        DEVICE_UNDER_TEST['host'],
-        log_level='DEBUG',
-        log_file='api.log'
-    )
-    username = DEVICE_UNDER_TEST['username']
-    passwd = DEVICE_UNDER_TEST['password']
-    client.login(username=username, password=passwd)
-    yield client
-    client.logout()
-
 
 class TestEvengApiLab:
     ''' Test cases '''
 
-    def test_api_get_lab_wo_extension(self, client):
+    def test_api_get_lab_wo_extension(self, authenticated_client):
         """
         Retrieve Lab details without file extension
         """
         fullpath = LAB_PATH + LAB_NAME
-        lab = client.api.get_lab(fullpath)
+        lab = authenticated_client.api.get_lab(fullpath)
         assert lab.get('name') == LAB_NAME
 
-    def test_api_get_lab_w_extension(self, client):
+    def test_api_get_lab_w_extension(self, authenticated_client):
         """
         Retrieve Lab details with file extension
         """
         fullpath = LAB_PATH + LAB_NAME + EXT
-        lab = client.api.get_lab(fullpath)
+        lab = authenticated_client.api.get_lab(fullpath)
         assert lab.get('name') == LAB_NAME
 
-    def test_get_non_existing_lab(self, client):
+    def test_get_non_existing_lab(self, authenticated_client):
         """
         Verify that retrieving a non existing lab
         raises an error.
         """
-        with pytest.raises(HTTPError):
-            client.api.get_lab('FAKE_LAB_PATH')
+        with pytest.raises(EvengHTTPError):
+            authenticated_client.api.get_lab('FAKE_LAB_PATH')
 
-    def test_create_lab(self, client):
-        resp = client.api.create_lab(**TESTLAB)
+    def test_create_lab(self, authenticated_client):
+        resp = authenticated_client.api.create_lab(**TESTLAB)
         assert resp['status'] == 'success'
 
-    def test_get_lab_topology(self, client):
+    def test_edit_lab(self, authenticated_client):
+        long_description = "longer description with edit"
         lab_path = TESTLAB['path'] + TESTLAB['name']
-        resp = client.api.get_lab(lab_path)
+        edited_param = {"body": long_description}
+        resp = authenticated_client.api.edit_lab(lab_path, edited_param)
+        assert resp["status"] == "success"
+
+    @pytest.mark.parametrize(
+        "params",
+        [
+            {"long_description": "test unknown param"},
+            {"name": "new_test", "description": "test"}
+        ]
+    )
+    def test_edit_lab_unsupported_params(self, authenticated_client, params):
+        with pytest.raises(ValueError):
+            lab_path = TESTLAB['path'] + TESTLAB['name']
+            authenticated_client.api.edit_lab(lab_path, params)
+
+    def test_lock_lab(self, authenticated_client):
+        lab_path = TESTLAB['path'] + TESTLAB['name']
+        resp = authenticated_client.api.lock_lab(lab_path)
+        assert resp["status"] == "success"
+
+    def test_unlock_lab(self, authenticated_client):
+        lab_path = TESTLAB['path'] + TESTLAB['name']
+        resp = authenticated_client.api.unlock_lab(lab_path)
+        assert resp["status"] == "success"
+
+    def test_get_lab_topology(self, authenticated_client):
+        lab_path = TESTLAB['path'] + TESTLAB['name']
+        resp = authenticated_client.api.get_lab(lab_path)
+        assert resp['name'] == TESTLAB['name']
+        lab_path = TESTLAB['path'] + TESTLAB['name']
+
+    def test_delete_lab(self, authenticated_client):
+        lab_path = TESTLAB['path'] + TESTLAB['name']
+        resp = authenticated_client.api.delete_lab(lab_path)
         assert resp['status'] == 'success'
