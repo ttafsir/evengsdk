f415d03fc8f11436e382a1e4c6a0b5d2fa2c50ce
Tafsir Thiam <ttafsir@gmail.com>
2022-01-05 08:43:39 -0500

    update version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     name="evengsdk",
     keywords="evengsdk",
     license="MIT license",
-    version="0.1.0",
+    version="0.1.1",
     author="Tafsir Thiam",
     author_email="ttafsir@gmail.com",
     description=(
