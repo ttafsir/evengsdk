0ae145e5e6fec678108060f1a905d101594b9000
Tafsir Thiam <ttafsir@gmail.com>
2022-01-06 22:12:00 -0500

    Adds console module to manage cli output

--- src/evengsdk/plugins/display.py
+++ src/evengsdk/plugins/display.py
@@ -2,10 +2,9 @@
 # standard lib imports
 import html
 import json as jsonlib
-from typing import List, Dict, Union
+from typing import List, Dict, Union, Any
 
 # third party lib imports
-import click
 from tabulate import tabulate
 
 # package imports
@@ -19,7 +18,7 @@ def register_plugin(func):
     return func
 
 
-def display(plugin: str, data: Union[List, Dict], *args, **kwargs) -> str:
+def format_output(plugin: str, data: Union[List, Dict], *args, **kwargs) -> str:
     if plugin in _PLUGINS:
         return _PLUGINS[plugin](data, *args, **kwargs)
     else:
@@ -29,8 +28,9 @@ def display(plugin: str, data: Union[List, Dict], *args, **kwargs) -> str:
 @register_plugin
 def json(data, *args, **kwargs):
     indent = kwargs.get("indent", 2)
-    fg_color = kwargs.get("fg_color", "bright_white")
-    return click.style(jsonlib.dumps(data, indent=indent), fg=fg_color)
+    if isinstance(data, dict):
+        return jsonlib.dumps(data.get("data", data), indent=indent)
+    return jsonlib.dumps(data, indent=indent)
 
 
 @register_plugin
@@ -65,43 +65,19 @@ def _dict_to_string(obj: dict) -> str:
 
 @register_plugin
 def text(
-    data: Dict,
-    header: List[str] = [],
-    fg_color: str = "bright_white",
-    record_header: str = "",
-    record_header_fg_color: str = "yellow",
+    data: Any,
+    *args,
+    **kwargs,
 ) -> str:
-    """Generate human readable output for passed object
-
-    Args:
-        data (Dict): dict or list of dicts to format as output
-        header (List[str], optional): list of keys to output. Defaults to [].
-        fg_color (str, optional): fg color for output. Defaults to
-            "bright_white".
-        record_header (str, optional): Header to display for each record in a
-            list. Defaults to "".
-
-    Returns:
-        str: formatted output string
-    """
-    string_output = "\n"
-
+    """Format data as text"""
     if isinstance(data, dict):
-        for ln in _dict_to_string(data):
-            string_output += ln
-
-    elif isinstance(data, list):
-        for obj in data:
-            if record_header:
-                string_output += click.style(
-                    obj[record_header].upper(), fg=record_header_fg_color
-                )
-                string_output += "\n"
-
-            if isinstance(obj, dict):
-                for ln in _dict_to_string(obj):
-                    string_output += ln
-                string_output += "\n"
-    else:
-        string_output += str(data)
-    return click.style(string_output, fg=fg_color)
+        string_output = ""
+        data_dict = data.get("data")
+        if data_dict:
+            for ln in _dict_to_string(data_dict):
+                string_output += ln
+            return string_output
+        elif data.get("message"):
+            string_output = f"{data.get('status')}: {data.get('message')}"
+            return string_output
+    return data
