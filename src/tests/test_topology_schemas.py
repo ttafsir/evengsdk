2f93b60404679ca00cf37004db63ba5f33256e45
Tafsir Thiam <ttafsir@gmail.com>
2022-01-13 15:47:28 -0500

    Adds `configuration` to node schema

--- src/tests/test_topology_schemas.py
+++ src/tests/test_topology_schemas.py
@@ -31,21 +31,82 @@ def validator(schema):
     return jsonschema.Draft7Validator(schema=schema)
 
 
-@pytest.mark.parametrize("filename", ["topology_missing_lab_name.yaml"])
+@pytest.mark.parametrize("filename", ["topology_lab_tests.yaml"])
 def test_missing_lab_name_raises(topology, validator):
     """
     Verify topology schema is valid
     """
+    test_topology = topology["missing_lab_name"]
     with pytest.raises(jsonschema.exceptions.ValidationError) as e:
-        validator.validate(topology)
+        validator.validate(test_topology)
     assert "'name' is a required property" in str(e.value)
 
 
-@pytest.mark.parametrize("filename", ["topology_empty_lab_name.yaml"])
+@pytest.mark.parametrize("filename", ["topology_lab_tests.yaml"])
 def test_null_lab_name_fails(topology, validator):
     """
     Verify topology schema is valid
     """
-    with pytest.raises(jsonschema.exceptions.ValidationError):
-        errors = validator.validate(topology)
-        assert "is not of type" in errors
+    test_topology = topology["empty_lab_name"]
+    with pytest.raises(jsonschema.exceptions.ValidationError) as e:
+        validator.validate(test_topology)
+    assert "is not of type" in str(e.value)
+
+
+@pytest.mark.parametrize("filename", ["topology_node_tests.yaml"])
+def test_missing_node_name_raises(topology, validator):
+    """
+    Verify topology schema is valid
+    """
+    test_topology = topology["missing_node_name"]
+    with pytest.raises(jsonschema.exceptions.ValidationError) as e:
+        validator.validate(test_topology)
+    assert "'name' is a required property" in str(e.value)
+    assert "'required' in schema['properties']['nodes']['items']" in str(e.value)
+
+
+@pytest.mark.parametrize("filename", ["topology_node_tests.yaml"])
+def test_missing_node_template_raises(topology, validator):
+    """
+    Verify topology schema is valid
+    """
+    test_topology = topology["missing_node_template"]
+    with pytest.raises(jsonschema.exceptions.ValidationError) as e:
+        validator.validate(test_topology)
+    assert "'template' is a required property" in str(e.value)
+    assert "'required' in schema['properties']['nodes']['items']" in str(e.value)
+
+
+@pytest.mark.parametrize("filename", ["topology_node_tests.yaml"])
+def test_missing_node_image_raises(topology, validator):
+    """
+    Verify topology schema is valid
+    """
+    test_topology = topology["missing_node_image"]
+    with pytest.raises(jsonschema.exceptions.ValidationError) as e:
+        validator.validate(test_topology)
+    assert "'image' is a required property" in str(e.value)
+    assert "'required' in schema['properties']['nodes']['items']" in str(e.value)
+
+
+@pytest.mark.parametrize("filename", ["topology_node_tests.yaml"])
+def test_node_config_null_raises(topology, validator):
+    """
+    Verify topology schema is valid
+    """
+    test_topology = topology["node_config_is_null"]
+    with pytest.raises(jsonschema.exceptions.ValidationError) as e:
+        validator.validate(test_topology)
+    assert "None is not valid under any of the given schemas" in str(e.value)
+    assert "On instance['nodes'][0]['configuration']" in str(e.value)
+
+
+@pytest.mark.parametrize("filename", ["topology_node_tests.yaml"])
+def test_node_config_invalid_type_raises(topology, validator):
+    """
+    Verify topology schema is valid
+    """
+    test_topology = topology["invalid_node_type"]
+    with pytest.raises(jsonschema.exceptions.ValidationError) as e:
+        validator.validate(test_topology)
+    assert "None is not valid under any of the given schemas" in str(e.value)
